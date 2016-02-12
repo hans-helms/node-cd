88f26d176aeb9387d4535e10c80c61394f3f3a08
Hans Helms <hans.helms@gmail.com>
2016-02-12 16:48:14 +0100

    changed bb

--- bitbucket.sh
+++ bitbucket.sh
@@ -12,8 +12,15 @@ reslog=$(git log HEAD..origin/master --oneline)
 if [ "${reslog}" != "" ] ; then
  echo "pulling changes from the server"
 
+    pm2 stop ./server.js
+    
     git pull
+
+    gulp clear-database
+    gulp update-database
     npm install
+    pm2 start ./server.js
+
 
 else
  echo "no git changes"
