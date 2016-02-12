c4dbee6efb5513205087f16fef261d3d6d43b6e5
Hans Helms <hans.helms@gmail.com>
2016-02-12 11:44:45 +0100

    xx

--- bitbucket.sh
+++ bitbucket.sh
@@ -1,2 +1,20 @@
-echo "Updating Template"
-echo "====================================="
+
+#!/bin/sh
+echo "starting bitbucket.sh"
+
+GIT_WORK_TREE=~/web/gtp.net/
+cd $GIT_WORK_TREE
+
+
+git fetch origin
+reslog=$(git log HEAD..origin/master --oneline)
+
+if [ "${reslog}" != "" ] ; then
+ echo "pulling changes from the server"
+
+    git pull
+    npm install
+
+else
+ echo "no git changes"
+fi
